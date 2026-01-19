ce35d7344164b096b6bb0064735e8c872c94d284
NathanosDev <contact@nathanos.dev>
2026-01-19 12:21:36 +0100

    add deployment pipelines

--- src/frontend/src/env.tsx
+++ src/frontend/src/env.tsx
@@ -10,6 +10,7 @@ function getEnvVar(name: string): string {
 
 const DFX_NETWORK = getEnvVar('DFX_NETWORK');
 const IS_LOCAL = DFX_NETWORK === 'local';
+const IS_TEST = DFX_NETWORK === 'test';
 const II_CANISTER_ID = getEnvVar('CANISTER_ID_INTERNET_IDENTITY');
 
 export const IDENTITY_PROVIDER = IS_LOCAL
@@ -20,6 +21,5 @@ export const BACKEND_CANISTER_ID: string = getEnvVar('CANISTER_ID_BACKEND');
 
 export const SHOULD_FETCH_ROOT_KEY = IS_LOCAL;
 
-export const DERIVATION_ORIGIN = IS_LOCAL
-  ? undefined
-  : 'https://console.subnet.ch';
+export const DERIVATION_ORIGIN =
+  IS_LOCAL || IS_TEST ? undefined : 'https://console.subnet.ch';
