163cfaf03d6d02f33ea896ba1440fee930e0cb48
NathanosDev <contact@nathanos.dev>
2026-04-09 21:16:56 +0200

    pr feedback

--- src/frontend/src/routes/redirect-to-project-canisters/redirect-to-project-canisters.tsx
+++ src/frontend/src/routes/redirect-to-project-canisters/redirect-to-project-canisters.tsx
@@ -2,19 +2,16 @@ import { useEffect, type FC } from 'react';
 import { useNavigate } from 'react-router';
 import { useAppStore } from '@/lib/store';
 import { useRequireAuth } from '@/lib/auth';
-import { isNil } from '@/lib/nil';
+import { isNil, isNotNil } from '@/lib/nil';
 
 const RedirectToCanisters: FC = () => {
   useRequireAuth();
   const navigate = useNavigate();
   const { projects } = useAppStore();
-  const defaultProject = projects[0];
-  if (isNil(defaultProject)) {
-    throw new Error('Default project not found');
-  }
 
   useEffect(() => {
-    if (projects.length > 0) {
+    const defaultProject = projects[0];
+    if (isNotNil(defaultProject)) {
       navigate(`/projects/${defaultProject.id}/canisters`, { replace: true });
     }
   }, [navigate, projects]);
